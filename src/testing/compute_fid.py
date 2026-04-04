8bb23c7620b28221bdf1d257ddf43e0f7bd1523c
Andreas Pommerencke <anpom21@student.sdu.dk>
2026-04-04 10:19:43 +0200

    FID script update

--- src/testing/compute_fid.py
+++ src/testing/compute_fid.py
@@ -45,18 +45,16 @@ def main():
     parser = argparse.ArgumentParser(description="Compute FID between real and synthetic image folders")
     parser.add_argument("--real", type=str, required=True, help="Path to real images")
     parser.add_argument("--fake", type=str, required=True, help="Path to synthetic images")
-    parser.add_argument("--cuda", action="store_true", help="Use CUDA if available")
-    parser.add_argument("--search_deep", action="store_true", help="Search for all images in subdirectories")
+    parser.add_argument("--dont_search_deep", action="store_false", help="Search for all images in subdirectories")
     parser.add_argument("--batch-size", type=int, default=64)
     args = parser.parse_args()
 
     compute_fid(
         real=args.real,
         fake=args.fake,
-        cuda=args.cuda,
-        search_deep=args.search_deep,
+        cuda=True,
+        search_deep=args.dont_search_deep,
         batch_size=args.batch_size,
-        compute_num=args.compute_num,
     )
 
 
